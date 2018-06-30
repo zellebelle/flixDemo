8cd0d7bcd7c5342287844e535f57e018689a1208
Gisele Phalo <zellebelle@fb.com>
2018-06-29 17:46:51 -0700

    here

---
+++ flixDemo/View Controllers/DetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailsViewController.h
+//  flixDemo
+//
+//  Created by Gisele Phalo on 6/29/18.
+//  Copyright © 2018 Gisele Phalo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailsViewController : UIViewController
+
+@end
